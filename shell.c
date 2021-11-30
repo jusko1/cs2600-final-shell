f2a49bad32d89a71c137162cc70f455a03973dd1
Justin <jko1@cpp.edu>
2021-11-30 15:54:38 -0800

    split_line(), and lsh_launch() with comments

--- shell.c
+++ shell.c
@@ -21,6 +21,7 @@ void lsh_loo(void){
     while (status);
 }
 
+#define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
     int bufsize = LSH_RL_BUFSIZE;
@@ -36,24 +37,86 @@ char *lsh_read_line(void)
     while (1){
         c = getchar();
 
-        //As long as the char is not the end of file or a newline,
+        //As long as the char is not the end of file or a newline character,
+        // if it is one of the two, return the string
         if (c == EOF || c == '\n') {
             buffer[position] = '\0';
             return buffer;
         }
+        // add the character the the existing string
         else {
             buffer[position] = c;
         }
         position ++;
 
-        //
+        //If the line takes up more space in memory than was allocated by the system, 
+        // reallocate memory and loop back. If no such buffer exists, exit with error
         if (position>=bufsize) {
             bufsize += LSH_RL_BUFSIZE;
-            buffer - realloc(buffer, bufsize);
+            buffer = realloc(buffer, bufsize);
             if(!buffer){
                 fprint(stderr, "lsh: allocation error \n");
                 exit(EXIT_FAILURE);
             }
         }
     }
+}
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM "\t\r\n\a"
+//splits the line into a list of arguments using whitespace as delimiters
+char **lsh_split_line(char *line){
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+
+    if (!tokens){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, LSH_TOK_DELIM);
+    //while the token is not null, adds it to the array
+    while(token != NULL) {
+        tokens[position] = token;
+        position++;
+        // if the memory allocated is not enough, reallocate more memory
+        if (position >= bufsize){
+            bufsize += LSH_TOCK-BUFSIZE;
+            tokens = realloc(token, bufsize * sizeof(char*));
+            if (!tokens) {
+                //if memory reallocation fails, prints error and exits.
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        //go to the next token
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    //the array is null-terminated
+    tokens[position] = NULL;
+    //returns an array of tokens
+    return tokens;
+}
+int lsh_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args ) == -1){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0) {
+        perror("lsh");
+    }
+    else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        
+        }
+        while (!WIFEXITED(status) && !WIFSIGNALLED(status));
+    }
+    return 1;
 }
